531fad0d762f89b434cdfc602a58b86c7cd91072
andrijac <andrija11@gmail.com>
2018-03-12 23:31:48 +0100

    init

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+	public partial class Form1 : Form
+	{
+		private Presenter presenter;
+
+		public Form1()
+		{
+			this.InitializeComponent();
+
+			this.presenter = new Presenter()
+			{
+				Name = "Foo",
+				SecondName = "Bar",
+				FormWidth = 572
+			};
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			this.textBox1.DataBindings.Add("Text", this.presenter, "Name");
+			this.DataBindings.Add("Width", this.presenter, "FormWidth");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			this.presenter.Name = "Drugo ime";
+		}
+
+		private void textBox2_TextChanged(object sender, EventArgs e)
+		{
+			int width;
+
+			if (int.TryParse(this.textBox2.Text, out width))
+			{
+				if (width < 209)
+				{
+					return;
+				}
+
+				this.presenter.FormWidth = width;
+			}
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			int test = this.presenter.FormWidth;
+			this.presenter.FormWidth = test + 10;
+		}
+	}
+}
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Presenter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace WindowsFormsApplication1
+{
+	public class Presenter : INotifyPropertyChanged
+	{
+		#region INotifyPropertyChanged Members
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		#endregion INotifyPropertyChanged Members
+
+		private string name;
+
+		private string secondName;
+
+		private int width;
+
+		protected virtual void OnPropertyChange(string propertyName)
+		{
+			if (this.PropertyChanged != null)
+			{
+				this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+
+		public string Name
+		{
+			get
+			{
+				return this.name;
+			}
+			set
+			{
+				this.name = value;
+				this.OnPropertyChange(this.GetPropertyName(() => this.Name));
+			}
+		}
+
+		public string SecondName
+		{
+			get
+			{
+				return this.secondName;
+			}
+			set
+			{
+				this.secondName = value;
+				this.OnPropertyChange(this.GetPropertyName(() => this.SecondName));
+			}
+		}
+
+		public int FormWidth
+		{
+			get
+			{
+				return this.width;
+			}
+			set
+			{
+				this.width = value;
+				this.OnPropertyChange(this.GetPropertyName(() => this.FormWidth));
+			}
+		}
+
+		#region TL;DR
+
+		protected virtual string GetPropertyName<T>(Expression<Func<T>> propertyExpression)
+		{
+			return ExtractPropertyName(this, propertyExpression);
+		}
+
+		/// <summary>
+		/// http://dotnet.dzone.com/articles/implementing
+		/// </summary>
+		/// <typeparam name="T"></typeparam>
+		/// <typeparam name="TObj"></typeparam>
+		/// <param name="obj"></param>
+		/// <param name="propertyExpression"></param>
+		/// <returns></returns>
+		public static string ExtractPropertyName<T, TObj>(TObj obj, Expression<Func<T>> propertyExpression)
+		{
+			if (propertyExpression == null)
+			{
+				throw new ArgumentNullException("propertyExpression");
+			}
+
+			// http://stackoverflow.com/questions/12975373/expression-for-type-members-results-in-different-expressions-memberexpression
+			UnaryExpression unaryExpression = propertyExpression.Body as UnaryExpression;
+			MemberExpression memberExpression = unaryExpression != null ?
+												unaryExpression.Operand as MemberExpression :
+												propertyExpression.Body as MemberExpression;
+
+			if (memberExpression == null)
+			{
+				throw new ArgumentException("The expression is not a member access expression.", "propertyExpression");
+			}
+
+			PropertyInfo property = memberExpression.Member as PropertyInfo;
+			if (property == null)
+			{
+				throw new ArgumentException("The member access expression does not access a property.", "propertyExpression");
+			}
+
+			if (!property.DeclaringType.IsAssignableFrom(obj.GetType()))
+			{
+				throw new ArgumentException("The referenced property belongs to a different type.", "propertyExpression");
+			}
+
+			MethodInfo getMethod = property.GetGetMethod(true);
+			if (getMethod == null)
+			{
+				// this shouldn't happen - the expression would reject the property before reaching this far
+				throw new ArgumentException("The referenced property does not have a get method.", "propertyExpression");
+			}
+
+			if (getMethod.IsStatic)
+			{
+				throw new ArgumentException("The referenced property is a static property.", "propertyExpression");
+			}
+
+			return memberExpression.Member.Name;
+		}
+
+		#endregion TL;DR
+	}
+}
